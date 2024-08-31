9f15431ea21fc361356c90cd15db1d31521af71a
Daniel Mark <danielmark.uc@gmail.com>
2024-08-31 23:07:40 +0530

    feat: MapBox places integration, Google maps reverse geocoding, location detection

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -1,12 +1,299 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { MapBox } from "@/components/MapBox";
+import { useEffect, useState } from "react";
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Label,
+} from "@headlessui/react";
+import { CheckIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+
+export interface IGitHubProfile {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  score: number;
+}
+
+// Function to reverse geocode and get the country code
+const reverseGeocode = async (latitude: number, longitude: number) => {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to reverse geocode location.");
+  }
+
+  const data = await response.json();
+  const addressComponents = data.results[0].address_components;
+
+  const countryComponent = addressComponents.find(
+    (component: { types: string | string[] }) =>
+      component.types.includes("country")
+  );
+
+  return countryComponent ? countryComponent.short_name : null;
+};
+
+// Function to fetch chargers from OpenChargeMap API by country
+const fetchChargersByCountry = async (countryCode: any) => {
+  const response = await fetch(
+    `https://api.openchargemap.io/v3/poi/?output=json&countrycode=${countryCode}&maxresults=100&key=${process.env.NEXT_PUBLIC_OPEN_CHARGE_API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch chargers.");
+  }
+
+  const chargers = await response.json();
+  return chargers;
+};
+
+// Function to calculate the distance between two coordinates using the Haversine formula
+const getDistanceFromLatLonInKm = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) => {
+  const R = 6371; // Radius of the Earth in km
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) *
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // Distance in km
+  return distance;
+};
 
 function DashboardPage() {
+  const [location, setLocation] = useState<any>(null);
+  const [countryCode, setCountryCode] = useState(null);
+  const [chargers, setChargers] = useState<any>([]);
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    if (chargers.length > 0) {
+      console.log("Chargers", chargers);
+    }
+  }, [chargers]);
+
+  // From
+  const [gitHubProfile, setGitHubProfile] = useState<IGitHubProfile>();
+  const [gitHubUsernameQuery, setGitHubUsernameQuery] = useState("");
+  const [gitHubProfilesList, setGitHubProfilesList] = useState([]);
+  const [filteredGitHubProfiles, setFilteredGitHubProfiles] = useState([]);
+
+  useEffect(() => {
+    if (gitHubProfilesList.length > 0) {
+      const filteredGitHubProfilesTemp = gitHubProfilesList.filter(
+        (githubProfile: { login: string }) => {
+          return githubProfile.login
+            .toLowerCase()
+            .includes(gitHubUsernameQuery.toLowerCase());
+        }
+      );
+
+      // eslint-disable-next-line no-console
+      console.log("Filtered GitHub Profiles: ", filteredGitHubProfilesTemp);
+      setFilteredGitHubProfiles(filteredGitHubProfilesTemp);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gitHubProfilesList]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      async function getUsers() {
+        fetch(`/api/github-search?username=${gitHubUsernameQuery}`).then(
+          (response) => {
+            response.json().then((data) => {
+              setGitHubProfilesList(data.results);
+            });
+          }
+        );
+      }
+      if (gitHubUsernameQuery) getUsers();
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [gitHubUsernameQuery]);
+
+  // Getting current user location
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          // setLocation({ latitude, longitude });
+
+          // Setting the location to Los Angeles for demo purposes
+          setLocation({ latitude: 33.8852882, longitude: -117.9787195 });
+
+          try {
+            // Reverse geocode to get the country code
+            // TODO: Uncomment this for production
+            // const countryCode = await reverseGeocode(latitude, longitude);
+            // setCountryCode(countryCode);
+
+            // Fetch chargers by country code
+            // TODO: Uncomment this for production
+            // const chargers = await fetchChargersByCountry(countryCode);
+            // TODO: Comment this out for production. We're setting the country to USA for demo purposes
+            const chargers = await fetchChargersByCountry("US");
+
+            // Sort chargers by distance from current location
+            const sortedChargers = chargers
+              .map(
+                (charger: {
+                  AddressInfo: { Latitude: number; Longitude: number };
+                }) => ({
+                  ...charger,
+                  distance: getDistanceFromLatLonInKm(
+                    latitude,
+                    longitude,
+                    charger.AddressInfo.Latitude,
+                    charger.AddressInfo.Longitude
+                  ),
+                })
+              )
+              .sort(
+                (a: { distance: number }, b: { distance: number }) =>
+                  a.distance - b.distance
+              );
+
+            setChargers(sortedChargers);
+          } catch (err) {
+            // setError(err.message);
+          }
+        },
+        (error) => {
+          setError(error.message);
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+    }
+  }, []);
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         setLocation({
+  //           latitude: position.coords.latitude,
+  //           longitude: position.coords.longitude,
+  //         });
+  //         console.log(
+  //           `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`
+  //         );
+  //       },
+  //       (error) => {
+  //         setError(error.message);
+  //       }
+  //     );
+  //   } else {
+  //     setError("Geolocation is not supported by this browser.");
+  //   }
+  // }, []);
+
   return (
-    <>
-      <MapBox />
-    </>
+    <div className="relative">
+      {/* <div className="absolute m-3 bg-zinc-900/70 rounded-xl overflow-hidden z-50">
+        <div className="max-w-3xl p-5 bg-black shadow-lg">
+          <div className="w-full">
+            <Combobox
+              as="div"
+              value={gitHubProfile}
+              onChange={(selectedGitHubProfile) => {
+                setGitHubUsernameQuery("");
+                setGitHubProfile(selectedGitHubProfile ?? undefined);
+              }}
+            >
+              <div className="relative">
+                <ComboboxInput
+                  className="w-full rounded-md border-0 bg-zinc-900 py-1.5 pl-3 pr-12 text-zinc-900 shadow-sm ring-1 ring-inset ring-zinc-800 focus:ring-2 focus:ring-inset focus:ring-purple-600 sm:text-sm sm:leading-6"
+                  onChange={(event) =>
+                    setGitHubUsernameQuery(event.target.value)
+                  }
+                  onBlur={() => setGitHubUsernameQuery("")}
+                  displayValue={(gitHubUser: { login?: string }) =>
+                    gitHubUser?.login || ""
+                  }
+                  placeholder="Type to search"
+                />
+                <ComboboxButton className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                  <MagnifyingGlassIcon
+                    className="h-5 w-5 text-zinc-400"
+                    aria-hidden="true"
+                  />
+                </ComboboxButton>
+
+                {filteredGitHubProfiles.length > 0 && (
+                  <ComboboxOptions className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {filteredGitHubProfiles.map(
+                      (gitHubProfile: IGitHubProfile) => (
+                        <ComboboxOption
+                          key={gitHubProfile.id}
+                          value={gitHubProfile}
+                          className="group relative cursor-default select-none py-2 pl-3 pr-9 text-zinc-900 data-[focus]:bg-purple-600 data-[focus]:text-white"
+                        >
+                          <div className="flex items-center">
+                            <img
+                              src={gitHubProfile.avatar_url}
+                              alt=""
+                              className="h-6 w-6 flex-shrink-0 rounded-full"
+                            />
+                            <span className="ml-3 truncate group-data-[selected]:font-semibold">
+                              {gitHubProfile.login}
+                            </span>
+                          </div>
+
+                          <span className="absolute inset-y-0 right-0 hidden items-center pr-4 text-purple-600 group-data-[selected]:flex group-data-[focus]:text-white">
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        </ComboboxOption>
+                      )
+                    )}
+                  </ComboboxOptions>
+                )}
+              </div>
+            </Combobox>
+          </div>
+        </div>
+      </div> */}
+
+      {/* Conditionally render the MapBox component only when location is set */}
+      {location && chargers.length > 0 && (
+        <MapBox location={location} chargers={chargers} />
+      )}
+
+      {/* Optionally, show an error message if there was an issue fetching the location */}
+      {error && <p className="text-red-500 text-center">{error}</p>}
+    </div>
   );
 }
 
--- web/components/MapBox.tsx
+++ web/components/MapBox.tsx
@@ -1,12 +1,191 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { useEffect, useState } from "react";
-import { GeolocateControl, Map, NavigationControl } from "react-map-gl";
+import { GeolocateControl, Map, NavigationControl, Marker } from "react-map-gl";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
 import "mapbox-gl/dist/mapbox-gl.css";
 
-const MapBox = () => {
-  const [viewport, setViewport] = useState([0, 0]);
+interface DataProviderStatusType {
+  IsProviderEnabled: boolean;
+  ID: number;
+  Title: string;
+}
 
+interface DataProvider {
+  WebsiteURL: string | null;
+  Comments: string | null;
+  DataProviderStatusType: DataProviderStatusType;
+  IsRestrictedEdit: boolean;
+  IsOpenDataLicensed: boolean;
+  IsApprovedImport: boolean;
+  License: string;
+  DateLastImported: string | null;
+  ID: number;
+  Title: string;
+}
+
+interface OperatorInfo {
+  WebsiteURL: string | null;
+  Comments: string | null;
+  PhonePrimaryContact: string | null;
+  PhoneSecondaryContact: string | null;
+  IsPrivateIndividual: boolean | null;
+  AddressInfo: string | null;
+  BookingURL: string | null;
+  ContactEmail: string | null;
+  FaultReportEmail: string | null;
+  IsRestrictedEdit: boolean | null;
+  ID: number;
+  Title: string;
+}
+
+interface UsageType {
+  IsPayAtLocation: boolean;
+  IsMembershipRequired: boolean;
+  IsAccessKeyRequired: boolean;
+  ID: number;
+  Title: string;
+}
+
+interface StatusType {
+  IsOperational: boolean;
+  IsUserSelectable: boolean;
+  ID: number;
+  Title: string;
+}
+
+interface SubmissionStatus {
+  IsLive: boolean;
+  ID: number;
+  Title: string;
+}
+
+interface Country {
+  ISOCode: string;
+  ContinentCode: string;
+  ID: number;
+  Title: string;
+}
+
+interface AddressInfo {
+  ID: number;
+  Title: string;
+  AddressLine1: string;
+  AddressLine2: string | null;
+  Town: string;
+  StateOrProvince: string;
+  Postcode: string;
+  CountryID: number;
+  Country: Country;
+  Latitude: number;
+  Longitude: number;
+  ContactTelephone1: string | null;
+  ContactTelephone2: string | null;
+  ContactEmail: string | null;
+  AccessComments: string | null;
+  RelatedURL: string;
+  Distance: number | null;
+  DistanceUnit: number;
+}
+
+interface ConnectionType {
+  FormalName: string;
+  IsDiscontinued: boolean;
+  IsObsolete: boolean;
+  ID: number;
+  Title: string;
+}
+
+interface CurrentType {
+  Description: string;
+  ID: number;
+  Title: string;
+}
+
+interface Level {
+  Comments: string;
+  IsFastChargeCapable: boolean;
+  ID: number;
+  Title: string;
+}
+
+interface StatusTypeConnection {
+  IsOperational: boolean;
+  IsUserSelectable: boolean;
+  ID: number;
+  Title: string;
+}
+
+interface Connection {
+  ID: number;
+  ConnectionTypeID: number;
+  ConnectionType: ConnectionType;
+  Reference: string | null;
+  StatusTypeID: number;
+  StatusType: StatusTypeConnection;
+  LevelID: number;
+  Level: Level;
+  Amps: number | null;
+  Voltage: number | null;
+  PowerKW: number;
+  CurrentTypeID: number;
+  CurrentType: CurrentType;
+  Quantity: number;
+  Comments: string | null;
+}
+
+interface ICharger {
+  DataProvider: DataProvider;
+  OperatorInfo: OperatorInfo;
+  UsageType: UsageType;
+  StatusType: StatusType;
+  SubmissionStatus: SubmissionStatus;
+  UserComments: string | null;
+  PercentageSimilarity: number | null;
+  MediaItems: any | null; // Use 'any' if you are not sure of the structure
+  IsRecentlyVerified: boolean;
+  DateLastVerified: string;
+  ID: number;
+  UUID: string;
+  ParentChargePointID: number | null;
+  DataProviderID: number;
+  DataProvidersReference: string | null;
+  OperatorID: number;
+  OperatorsReference: string | null;
+  UsageTypeID: number;
+  UsageCost: string | null;
+  AddressInfo: AddressInfo;
+  Connections: Connection[];
+  NumberOfPoints: number;
+  GeneralComments: string | null;
+  DatePlanned: string | null;
+  DateLastConfirmed: string | null;
+  StatusTypeID: number;
+  DateLastStatusUpdate: string;
+  MetadataValues: any | null; // Use 'any' if you are not sure of the structure
+  DataQualityLevel: number;
+  DateCreated: string;
+  SubmissionStatusTypeID: number;
+  distance: number;
+}
+
+const MapBox = ({
+  location,
+  chargers,
+}: {
+  location: { latitude: number; longitude: number };
+  chargers: Array<any>;
+}) => {
+  const [viewport, setViewport] = useState([0, 0]);
+  // const [addressOnMap, setAddressOnMap] = useState(null);
+  // const [markers, setMarkers] = useState([]);
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [PopUpData, setPopUpData] = useState<ICharger>();
+
+  // Use effect to get the viewport dimensions
   useEffect(() => {
     function getViewport() {
       let viewPortWidth;
@@ -29,26 +208,231 @@ const MapBox = () => {
     };
   }, []);
 
+  // Sample markers
+  const markers = [
+    {
+      firstName: "Daniel",
+      lastName: "Mark",
+      address: "Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+      latitude: 13.083215,
+      longitude: 80.237986,
+      chargerCapacity: "120",
+      title:
+        "New Charging Station At Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+    },
+    {
+      firstName: "Daniel",
+      lastName: "Mark",
+      address:
+        "Aspiran Garden Colony, Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+      latitude: 13.085186,
+      longitude: 80.237952,
+      chargerCapacity: "150",
+      title:
+        "New Charging Station At Aspiran Garden Colony, Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+    },
+    {
+      firstName: "Fabian",
+      lastName: "Ferno",
+      address:
+        "Mandapam Road, Aspiran Garden Colony, 600010, Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+      latitude: 13.0845332,
+      longitude: 80.2358215,
+      chargerCapacity: "200",
+      title:
+        "New Charging Station At Mandapam Road, Aspiran Garden Colony, 600010, Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+    },
+    {
+      firstName: "Richard",
+      lastName: "Hendricks",
+      address:
+        "Central Street, Kilpauk Garden Colony, 600010, Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+      latitude: 13.0849401,
+      longitude: 80.2339489,
+      chargerCapacity: "200",
+      title:
+        "New Charging Station At Central Street, Kilpauk Garden Colony, 600010, Kilpauk, Chennai, Chennai, Tamil Nadu, India",
+    },
+  ];
+
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(retrieveMarkers, error);
+  //   } else {
+  //     console.log("Geolocation not supported");
+  //   }
+  // }, []);
+
+  // function retrieveMarkers(position) {
+  //   const latitude = position.coords.latitude;
+  //   const longitude = position.coords.longitude;
+  //   setLocation([latitude, longitude]);
+  //   console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+  //   // Make API call to MapBox
+  //   fetch(
+  //     `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_API_KEY}`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       var addressTemp = data.features.filter((item) =>
+  //         item.place_type.includes("address")
+  //       )[0];
+  //       setAddressOnMap(addressTemp.place_name);
+  //     })
+  //     .then(() => {
+  //       // Get markers
+  //       axios
+  //         .get("/api/proposals")
+  //         .then((res) => {
+  //           console.log(
+  //             res.data.proposals.map((item) => JSON.parse(item["args"][8]))
+  //           );
+  //           setMarkers(
+  //             res.data.proposals.map((item) => JSON.parse(item["args"][8]))
+  //           );
+  //           console.log(markers);
+  //         })
+  //         .catch((err) => {
+  //           console.log(err);
+  //         });
+  //     })
+  //     .catch((error) => console.log(error));
+  // }
+
+  // function error() {
+  //   console.log("Unable to retrieve your location");
+  // }
+
   if (viewport[0] === 0 && viewport[1] === 0) {
     // Return null or a loading indicator while the viewport is being calculated
     return null;
   }
 
   return (
-    <div className="w-full">
-      <Map
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
-        initialViewState={{
-          longitude: -118.277316,
-          latitude: 34.0427325,
-          zoom: 12,
-        }}
-        style={{ width: viewport[0], height: viewport[1] }}
-        mapStyle="mapbox://styles/thedanielmark/cm0e8dvwt00vb01qs9uzz5r8o"
-      >
-        <GeolocateControl />
-        <NavigationControl />
-      </Map>
+    <div className="w-full overflow-hidden">
+      {location && (
+        <Map
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
+          attributionControl={false}
+          initialViewState={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            zoom: 8,
+          }}
+          style={{ width: viewport[0], height: viewport[1] }}
+          mapStyle="mapbox://styles/thedanielmark/cm0e8dvwt00vb01qs9uzz5r8o"
+        >
+          {console.log(location.latitude, location.longitude)}
+          <GeolocateControl />
+          <NavigationControl />
+          {chargers.map((charger: ICharger, index: number) => (
+            <Marker
+              key={index}
+              longitude={charger.AddressInfo.Longitude}
+              latitude={charger.AddressInfo.Latitude}
+              anchor="bottom"
+              onClick={() => {
+                setPopUpData(charger);
+                setShowPopUp(true);
+              }}
+              color="#0ea5e9"
+            />
+          ))}
+          {showPopUp && (
+            <div className="absolute top-3 left-3 bg-black px-5 py-3 rounded-md shadow-lg border border-zinc-800">
+              <XMarkIcon
+                className="h-6 w-6 text-zinc-400 absolute top-3 right-3 cursor-pointer"
+                aria-hidden="true"
+                onClick={() => setShowPopUp(false)}
+              />
+              {PopUpData?.AddressInfo.AddressLine1 && (
+                <div className="font-semibold text-white tracking-tight text-xl pr-8 max-w-lg">
+                  {PopUpData?.AddressInfo.AddressLine1}
+                </div>
+              )}
+              {PopUpData?.AddressInfo.Title && (
+                <div className="text-zinc-400 text-sm">
+                  {PopUpData?.AddressInfo.Title}
+                </div>
+              )}
+
+              <div className="my-3 border-b border-zinc-800" />
+
+              <div className="font-medium text-zinc-200 text-sm">
+                Charging Unit(s)
+              </div>
+              <div className="mt-2 flex items-center gap-x-2 font-medium text-zinc-500 text-sm">
+                <img
+                  src="/logo.png"
+                  width={100}
+                  height={100}
+                  alt="Logo"
+                  className="h-4 w-4"
+                />
+                <span>
+                  {/* Print out all the connections here */}
+                  {PopUpData?.Connections.map((connection) => (
+                    <span key={connection.ID}>{connection.Level.Title}</span>
+                  ))}
+                </span>
+              </div>
+
+              <div className="my-3 border-b border-zinc-800" />
+
+              <div className="font-medium text-zinc-200 text-base">Address</div>
+              <table className="mt-1">
+                <tbody>
+                  {PopUpData?.AddressInfo.Town && (
+                    <tr>
+                      <td className="text-zinc-500 text-sm">Town</td>
+                      <td className="text-zinc-200 text-sm pl-3">
+                        {PopUpData?.AddressInfo.Town}
+                      </td>
+                    </tr>
+                  )}
+                  {PopUpData?.AddressInfo.StateOrProvince && (
+                    <tr>
+                      <td className="text-zinc-500 text-sm">State/Province</td>
+                      <td className="text-zinc-200 text-sm pl-3">
+                        {PopUpData?.AddressInfo.StateOrProvince}
+                      </td>
+                    </tr>
+                  )}
+                  {PopUpData?.AddressInfo.Postcode && (
+                    <tr>
+                      <td className="text-zinc-500 text-sm">Postcode</td>
+                      <td className="text-zinc-200 text-sm pl-3">
+                        {PopUpData?.AddressInfo.Postcode}
+                      </td>
+                    </tr>
+                  )}
+                  {PopUpData?.AddressInfo.Country.Title && (
+                    <tr>
+                      <td className="text-zinc-500 text-sm">Country</td>
+                      <td className="text-zinc-200 text-sm pl-3">
+                        {PopUpData?.AddressInfo.Country.Title}
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+
+              <div className="mt-8 mb-3">
+                <Link
+                  href={`https://www.google.com/maps/search/?api=1&query=${PopUpData?.AddressInfo.Latitude}%2C${PopUpData?.AddressInfo.Longitude}`}
+                  target="_blank"
+                  passHref={true}
+                  className="w-full block text-center rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+                >
+                  Start Driving
+                </Link>
+              </div>
+            </div>
+          )}
+        </Map>
+      )}
     </div>
   );
 };
